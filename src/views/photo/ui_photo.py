90ca904d42ccd5c472c4c70c7071410901b659ec
ddmoyu <daydaymoyu@gmail.com>
2024-11-14 15:42:06 +0800

    feat: 添加 uic 程序

--- src/views/photo/ui_photo.py
+++ src/views/photo/ui_photo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'photozVvBKr.ui'
+## Form generated from reading UI file 'photo.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.8.0
 ##
--- src/views/video/ui_video.py
+++ src/views/video/ui_video.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'videouIMeuI.ui'
+## Form generated from reading UI file 'video.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.8.0
 ##
