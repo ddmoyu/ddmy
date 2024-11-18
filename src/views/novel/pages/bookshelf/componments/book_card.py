924c737ff306ad5b3e648edb5343e75b8c9e0867
ddmoyu <daydaymoyu@gmail.com>
2024-11-18 23:36:03 +0800

    书架与管理界面功能增强，增加鼠标右键

--- src/views/novel/pages/bookshelf/componments/book_card.py
+++ src/views/novel/pages/bookshelf/componments/book_card.py
@@ -1,13 +1,14 @@
 from PySide6.QtWidgets import QVBoxLayout
 from PySide6.QtCore import Qt, Signal, QSize
-
-
 from qfluentwidgets import (
     ImageLabel,
     ElevatedCardWidget,
     BodyLabel,
     ToolTipFilter,
     ToolTipPosition,
+    RoundMenu,
+    FluentIcon,
+    Action,
 )
 
 
@@ -17,6 +18,7 @@ class BookCard(ElevatedCardWidget):
     def __init__(self, parent=None, book_data=None) -> None:
         super().__init__(parent)
         self.book_data = book_data
+        self.setCursor(Qt.CursorShape.PointingHandCursor)
 
         main_layout = QVBoxLayout(self)
         main_layout.setContentsMargins(2, 2, 2, 16)
@@ -44,6 +46,38 @@ class BookCard(ElevatedCardWidget):
         title_label.setWordWrap(False)
         main_layout.addWidget(title_label)
 
+        self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.contextMenuEvent)
+
     def mouseReleaseEvent(self, e):
+        print("mouseReleaseEvent")
         if e.button() == Qt.MouseButton.LeftButton:
             self.book_clicked.emit(self.book_data)
+
+    def contextMenuEvent(self, pos):
+        menu = RoundMenu()
+        read_action = Action(FluentIcon.DOCUMENT, "阅读", self)
+        detail_action = Action(FluentIcon.DICTIONARY, "详情", self)
+        delete_action = Action(FluentIcon.DELETE, "删除", self)
+        change_action = Action(FluentIcon.GLOBE, "换源", self)
+        read_action.triggered.connect(self.on_read_triggered)
+        detail_action.triggered.connect(self.on_detail_triggered)
+        delete_action.triggered.connect(self.on_delete_triggered)
+        change_action.triggered.connect(self.on_change_triggered)
+        menu.addAction(read_action)
+        menu.addAction(detail_action)
+        menu.addAction(delete_action)
+        menu.addAction(change_action)
+        menu.exec_(self.mapToGlobal(pos), ani=True)
+
+    def on_read_triggered(self):
+        print("on_read_action_triggered")
+
+    def on_detail_triggered(self):
+        print("on_detail_action_triggered")
+
+    def on_delete_triggered(self):
+        print("on_delete_action_triggered")
+
+    def on_change_triggered(self):
+        print("on_change_action_triggered")
--- src/views/novel/pages/manager/manager.py
+++ src/views/novel/pages/manager/manager.py
@@ -3,15 +3,22 @@ import asyncio
 from .ui_manager import Ui_NovelManager
 from PySide6.QtWidgets import QWidget, QTableWidgetItem, QHeaderView, QAbstractItemView
 from PySide6.QtCore import Qt, QPoint
-from qfluentwidgets import InfoBar, InfoBarPosition, RoundMenu, Action, MenuAnimationType
+from qfluentwidgets import (
+    InfoBar,
+    InfoBarPosition,
+    RoundMenu,
+    Action,
+    MenuAnimationType,
+)
 from src.views.novel.utils.utils import load_json
 from qasync import Slot
 from src.views.novel.utils.utils import fetch_book_sources
 from .components.importInternet import ImportMsgbox
 
+
 class NovelManager(Ui_NovelManager, QWidget):
     def __init__(self, parent=None):
-        super().__init__(parent = parent)
+        super().__init__(parent=parent)
         self.parent = parent
         self.setupUi(self)
         self.init_ui()
@@ -20,13 +27,15 @@ class NovelManager(Ui_NovelManager, QWidget):
 
     def init_ui(self):
         headers = ["名称", "分组", "链接", "状态"]
-        self.table_sources.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        self.table_sources.horizontalHeader().setSectionResizeMode(
+            QHeaderView.ResizeMode.Stretch
+        )
         self.table_sources.setColumnCount(len(headers))
         self.table_sources.verticalHeader().hide()
         self.table_sources.setHorizontalHeaderLabels(headers)
         self.table_sources.setSelectRightClickedRow(True)
         self.table_sources.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
-        self.table_sources.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.table_sources.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
         self.table_sources.customContextMenuRequested.connect(self.show_context_menu)
 
     def init_signal(self):
@@ -56,7 +65,6 @@ class NovelManager(Ui_NovelManager, QWidget):
     def right_menu_clicked(self, item_text):
         print("clicked item", item_text)
 
-
     def toggle_item(self):
         print("toggle item")
 
@@ -66,7 +74,6 @@ class NovelManager(Ui_NovelManager, QWidget):
     def modify_item(self):
         print("modify item")
 
-
     def render_sources_table(self):
         self.table_sources.clearContents()
         sources = load_json("novel_sources.json")
@@ -75,13 +82,16 @@ class NovelManager(Ui_NovelManager, QWidget):
 
         self.table_sources.setRowCount(len(sources))
         for row, source in enumerate(sources):
-            self.table_sources.setItem(row, 0, QTableWidgetItem(source["bookSourceName"]))
+            self.table_sources.setItem(
+                row, 0, QTableWidgetItem(source["bookSourceName"])
+            )
             group = source.get("bookSourceGroup", "未分组")
             self.table_sources.setItem(row, 1, QTableWidgetItem(group))
-            self.table_sources.setItem(row, 2, QTableWidgetItem(source["bookSourceUrl"]))
+            self.table_sources.setItem(
+                row, 2, QTableWidgetItem(source["bookSourceUrl"])
+            )
             status = "启用" if source.get("enabled", False) else "禁用"
             self.table_sources.setItem(row, 3, QTableWidgetItem(status))
-
 
     def on_import_from_internet(self):
         asyncio.create_task(self.import_feeds_internet())
@@ -92,13 +102,23 @@ class NovelManager(Ui_NovelManager, QWidget):
         if w.exec():
             url = w.urlLineEdit.text()
             if not url:
-                InfoBar.info("", "请输入正确的链接", parent=self, position=InfoBarPosition.BOTTOM)
+                InfoBar.info(
+                    "", "请输入正确的链接", parent=self, position=InfoBarPosition.BOTTOM
+                )
                 return
-            InfoBar.info("", "正在导入...", parent=self, position=InfoBarPosition.BOTTOM)
+            InfoBar.info(
+                "", "正在导入...", parent=self, position=InfoBarPosition.BOTTOM
+            )
             list = await fetch_book_sources(url, True)
             if not list:
-                InfoBar.error("", "没有找到任何资源", parent=self, position=InfoBarPosition.BOTTOM)
+                InfoBar.error(
+                    "", "没有找到任何资源", parent=self, position=InfoBarPosition.BOTTOM
+                )
                 return
-            InfoBar.success("", "导入成功, 共{}个".format(len(list)), parent=self, position=InfoBarPosition.BOTTOM)
+            InfoBar.success(
+                "",
+                "导入成功, 共{}个".format(len(list)),
+                parent=self,
+                position=InfoBarPosition.BOTTOM,
+            )
             self.render_sources_table()
-
